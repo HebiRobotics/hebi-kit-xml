54ef3b28f0b79601bbc367a1f1e9d69c5358e7e8
Hardik Singh <singh.hardik1997@gmail.com>
2020-08-10 19:10:27 -0400

    Trying the script approach possibility

--- github-actions/hrdf_auto_pr.sh
+++ github-actions/hrdf_auto_pr.sh
@@ -1,8 +1,16 @@
 #!/bin/sh -l
 
-sh -c "echo Starting Auto-PR process"
+echo "This is the new action"
+time=$(date)
+echo "::set-output name=time::$time"
+
+ls
+
+# sh -c "echo Starting Auto-PR process"
 
 # sh -c "mkdir test-folder"
 
-sh -c "echo Completed Auto-PR process"
+# sh -c "echo Completed Auto-PR process"
+
+
 
